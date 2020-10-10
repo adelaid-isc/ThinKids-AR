787c8fc0c9d67770b560ed4e1931cc84b1cc77ee
Adelaid Acevedo Cardona <adelaid.acevedo16@tectijuana.edu.mx>
2020-10-09 22:01:25 -0700

    Comandos Básicos C# Script

---
+++ comandosBasicos.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class comandosBasicos : MonoBehaviour
+{
+    public void cargarEscena (string nombreEscena)
+    {  
+        SceneManager.LoadScene(nombreEscena);
+    }
+
+    public void resetearPuntuacion()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
